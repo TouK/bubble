996b2d8bd3faaae5c97065db37f9f887a7fb1dca
Pawel Byszewski <pawel.byszewski@ext.allegro.pl>
2015-10-02 14:08:02 +0200

    Initial source commit

---
+++ sample/src/main/java/pl/touk/android/bubble/sample/BubbleSampleActivity.kt
@@ -0,0 +1,12 @@
+package pl.touk.android.bubble.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BubbleSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bubble_sample)
+    }
+}

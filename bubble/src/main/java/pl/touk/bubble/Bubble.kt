385f0f8b4515b77ce8c0d9ecd7e1ac73aca5119a
Pawel Byszewski <pawel.byszewski@ext.allegro.pl>
2015-10-02 15:21:05 +0200

    kotlin library module POC

---
+++ bubble/src/main/java/pl/touk/bubble/Bubble.kt
@@ -0,0 +1,9 @@
+package pl.touk.bubble
+
+
+class Bubble {
+
+    public fun register(): String {
+        return "Bubble activated"
+    }
+}
--- sample/src/main/java/pl/touk/android/bubble/sample/BubbleSampleActivity.kt
+++ sample/src/main/java/pl/touk/android/bubble/sample/BubbleSampleActivity.kt
@@ -2,11 +2,16 @@ package pl.touk.android.bubble.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import pl.touk.bubble.Bubble
 
 class BubbleSampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bubble = Bubble();
         setContentView(R.layout.activity_bubble_sample)
+        val textView = findViewById(R.id.label) as TextView
+        textView.setText(bubble.register())
     }
 }

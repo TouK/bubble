64ead0a056cb03319f00502de6fba1dac4d46120
Pawel Byszewski <pawel.byszewski@ext.allegro.pl>
2015-10-12 22:58:28 +0200

    refactor

--- sample/src/main/java/pl/touk/android/bubble/sample/BubbleSampleActivity.kt
+++ sample/src/main/java/pl/touk/android/bubble/sample/BubbleSampleActivity.kt
@@ -2,20 +2,54 @@ package pl.touk.android.bubble.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
 import android.widget.TextView
 import pl.touk.android.bubble.Bubble
 import pl.touk.android.bubble.BubbleEvent
-import pl.touk.android.bubble.sample.R
+import pl.touk.android.bubble.orientation.Orientation
 
 class BubbleSampleActivity : AppCompatActivity() {
 
+    val ANIMATION_DURATION: Long = 500
+
     val bubble: Bubble = Bubble()
+    lateinit var textView: TextView
+    var startAngle = 0f
+    var endAngle = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bubble_sample)
-        val textView = findViewById(R.id.label) as TextView
+        textView = findViewById(R.id.label) as TextView
         bubble.register(this)
-            .subscribe { bubbleEvent: BubbleEvent -> textView.text = bubbleEvent.orientation.name() }
+            .subscribe { bubbleEvent: BubbleEvent -> rotateTo(bubbleEvent.orientation) }
+    }
+
+    private fun rotateTo(orientation: Orientation) {
+        val animSet = AnimationSet(true)
+        animSet.interpolator = DecelerateInterpolator()
+        animSet.fillAfter = true
+        animSet.isFillEnabled = true
+
+        val animRotate = RotateAnimation(startAngle, extractEndAngle(orientation),
+                                            Animation.RELATIVE_TO_SELF, 0.5f,
+                                            Animation.RELATIVE_TO_SELF, 0.5f)
+
+        animRotate.duration = ANIMATION_DURATION
+        animRotate.fillAfter = true
+        animSet.addAnimation(animRotate)
+        startAngle = extractEndAngle(orientation)
+        textView.startAnimation(animSet)
+    }
+
+    private fun extractEndAngle(orientation: Orientation): Float {
+        return when(orientation) {
+            Orientation.PORTRAIT -> 0f
+            Orientation.LANDSCAPE -> 90f
+            else -> endAngle
+        }
     }
 }
